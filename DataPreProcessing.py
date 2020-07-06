0503cd64dc48aa83d706063199d2e9bb941c625e
Moris-IIIT <morisjhon2018.1@gmail.com>
2020-07-06 21:27:26 +0530

    Preprocessing the dataset.

--- DataPreProcessing.py
+++ DataPreProcessing.py
@@ -1,2 +1,10 @@
-from skimage.io import imread_collection
-
+from skimage.io import imread_collection,concatenate_images
+import pandas as pd
+import numpy as np
+# Collection directory :
+directory_train="Data/train/*.png"
+directory_test="Data/test/*.png"
+# Importing Images : Creating a collection
+train_data=imread_collection(directory_train)
+test_data=imread_collection(directory_test)
+# Converting to array
